b0eab97f1eb833e1c994f842ae85006a778da344
anyaholovach <anya_back-end@ribs.studio>
2024-12-19 17:25:55 +0200

    added ci pipeline

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/health', (req, res) => {
+  res.status(200).send('Server is up and running!');
+});
+
 app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
@@ -25,8 +30,8 @@ app.use((req, res, next) => {
     });
     next();
 });
-app.use('/', router)
 
+app.use('/', router)
 
 const start = async() => {
     try {
